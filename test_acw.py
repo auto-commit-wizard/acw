38d98cccfff254cd9be151adb2ad50ffcc777f55
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-01 17:10:01 +0900

    test: Edit test_acw.py
    - Edit test_should_input_open_ai_api_key_when_config

--- test_acw.py
+++ test_acw.py
@@ -26,10 +26,12 @@ class ACWTest(TestCase):
     def test_should_input_open_ai_api_key_when_config(self):
         home_directory = self.get_mock_home_directory()
         acw = ACW(check_subcommands=False, home_directory=home_directory)
+        open_ai_api_key = "open_ai_api_key"
         dummy_open_ai_api_key = "dummy_open_ai_api_key"
         with patch("builtins.input", return_value=dummy_open_ai_api_key):
             acw.config()
 
         with open(self.get_mock_file_path(), "r") as f:
             config_map = self.parse_config_file_to_dict(f.read())
-            self.assertEqual(dummy_open_ai_api_key, config_map["open_ai_api_key"])
+
+        self.assertEqual(dummy_open_ai_api_key, config_map[open_ai_api_key])
