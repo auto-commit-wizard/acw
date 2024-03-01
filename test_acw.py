a187076e07d6e32e6576b3e30c8425a23f9b2345
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-01 17:10:40 +0900

    test: Edit test_acw.py
    - Add test_should_update_config_when_edit_config_is_true

--- test_acw.py
+++ test_acw.py
@@ -35,3 +35,43 @@ class ACWTest(TestCase):
             config_map = self.parse_config_file_to_dict(f.read())
 
         self.assertEqual(dummy_open_ai_api_key, config_map[open_ai_api_key])
+
+    def test_should_update_config_when_edit_config_is_true(self):
+        # given
+        home_directory = self.get_mock_home_directory()
+        acw = ACW(check_subcommands=False, home_directory=home_directory)
+        open_ai_api_key = "open_ai_api_key"
+        dummy_open_ai_api_key = "dummy_open_ai_api_key"
+        with patch("builtins.input", return_value=dummy_open_ai_api_key):
+            acw.config()
+
+        with open(self.get_mock_file_path(), "r") as f:
+            config_map = self.parse_config_file_to_dict(f.read())
+
+        self.assertEqual(dummy_open_ai_api_key, config_map[open_ai_api_key])
+        updated_dummy_open_ai_api_key = "updated_dummy_open_ai_api_key"
+
+        # when
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = [
+                updated_dummy_open_ai_api_key,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
+            ]
+            acw.config(edit_config=True)
+
+        # then
+        with open(self.get_mock_file_path(), "r") as f:
+            updated_config_map = self.parse_config_file_to_dict(f.read())
+        self.assertEqual(
+            updated_dummy_open_ai_api_key, updated_config_map[open_ai_api_key]
+        )
+        config_map.pop(open_ai_api_key)
+        updated_config_map.pop(open_ai_api_key)
+        self.assertEqual(config_map, updated_config_map)
