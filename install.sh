4567d0e0840bffc2af9c3b814c9320cd07936ef8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-03-14 03:55:00 +0900

    fix: Edit install.sh
    - Python 3.11 또는 3.12 버전이 설치되어 있는지 확인
      - 설치되어 있지 않으면 종료
    - Poetry 설치 여부 확인
      - 설치되어 있지 않으면 종료

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Poetry가 설치되어 있는지 확인
-if ! command -v poetry &> /dev/null
-then
-    echo "Poetry가 설치되어 있지 않습니다. 설치를 시작합니다..."
-    # 공식 설치 스크립트 사용
-    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-    
-    # 설치 후 PATH에 poetry를 추가할 수 있도록 환경 변수 설정
-    # 이 부분은 사용자의 쉘 설정에 따라 다를 수 있으며, bash를 예로 들었습니다.
-    source $HOME/.poetry/env
+# Python 3.11 또는 3.12 버전이 설치되어 있는지 확인
+if command -v python3.11 &>/dev/null || command -v python3.12 &>/dev/null; then
+  echo "Python 3.11 또는 3.12 버전이 설치되어 있습니다."
 else
-    echo "Poetry가 이미 설치되어 있습니다."
+  echo "Python 3.11 또는 3.12 버전이 설치되어 있지 않습니다."
+  exit 1
+fi
+
+# Poetry 설치 여부 확인
+if command -v poetry &>/dev/null; then
+  echo "Poetry가 이미 설치되어 있습니다."
+else
+  echo "Poetry가 설치되어 있지 않습니다."
+  echo "Poetry를 설치하려면 아래 링크를 참조하세요:"
+  echo "https://python-poetry.org/docs/#installation"
+  exit 1
 fi
 
 # Poetry를 사용하여 프로젝트 의존성 설치
